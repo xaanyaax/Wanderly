696e5035c8b7e3eefa110189e7b730d89b318d22
xaanyaax <aanyasingh282004@gmail.com>
2025-06-23 14:45:05 +0530

    added some routes and controllers

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import connectDB from "./src/database/connect.js";
 import * as dotenv from "dotenv";
 
+
+
 dotenv.config();
 
 const app = express();
@@ -12,11 +14,15 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors())
 
+//Routes:
+import postRoutes from "./src/routes/posts.js";
+app.use("/posts", postRoutes);  //every route starts with posts  local::host//8080/posts
+
 
 const startServer = async () => {
     try {
       await connectDB(process.env.MONGODB_URL); // Don't forget the await
-      app.listen(8080, () => console.log('Server started on port 8080'));
+      app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
     } catch (error) {
       console.log(error);
     }
