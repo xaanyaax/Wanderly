c248ecf045aec4f953f1e2ee8c803d462ea6f017
xaanyaax <aanyasingh282004@gmail.com>
2025-06-23 19:08:47 +0530

    i commit to this

---
+++ Client/src/components/Posts/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+      
+        <h1>Post Component</h1>
+    </div>
+  )
+}
+
+export default Post
---
+++ Client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Post from './Post/Post'
+
+function Posts() {
+  return (
+    <div>
+      
+        <h1>Posts Component</h1>
+        <Post />
+        <Post />
+        
+    </div>
+  )
+}
+
+export default Posts
