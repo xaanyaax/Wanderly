b8387c384994013f0098abdc249f6b0069c32130
xaanyaax <aanyasingh282004@gmail.com>
2025-06-27 16:32:14 +0530

    fixed some bugs

--- Server/src/middlewares/authMiddleware.js
+++ Server/src/middlewares/authMiddleware.js
@@ -3,13 +3,20 @@ import jwt from "jsonwebtoken";
 export const protect = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) return res.status(401).json({ message: "Not authorized" });
+  if (!token) {
+    const error = new Error("Not authorized, token missing");
+    error.status = 401;
+    return next(error); 
+  }
 
   try {
     const decoded = jwt.verify(token, "jwt_secret_key");
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: "Token failed", error: err.message });
+    const error = new Error("Token verification failed");
+    error.status = 401;
+    error.extraDetails = err.message;
+    next(error); 
   }
 };
