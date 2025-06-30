98083a21a5d3b6aad2000cf2d8487a510270cfa0
xaanyaax <aanyasingh282004@gmail.com>
2025-06-30 15:08:18 +0530

    added protected routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import Contact from "./pages/Contact.jsx";
-import {Routes , Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import SignUpPage from "./pages/SignUp.jsx";
 import LoginPage from "./pages/Login.jsx";
@@ -7,34 +7,31 @@ import Home from "./pages/Home.jsx";
 import Create from "./pages/Create.jsx";
 import Explore from "./pages/Explore.jsx";
 import Profile from "./pages/Profile.jsx";
-
-
-
-
+// import ProtectedRoute from "./components/protected/ProtectedRoute.jsx";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-      <Route path="/" element={<Home />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="/" element={<Home />} />
 
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/create" element={<Create />} />
 
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="*" element={<div>404 Not Found</div>} />
-      <Route path="/profile" element={<Profile />} />
-
-
-      
-      <Route path="/create" element={<Create />} />
-
-
-      <Route path="/explore" element={<Explore />} />
-      
-
-
+        {/* <Route
+          path="/create"
+          element={
+            <ProtectedRoute>
+              <Create />
+            </ProtectedRoute>
+          }
+        /> */}
       </Routes>
     </div>
   );
